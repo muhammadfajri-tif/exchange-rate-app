93ca9a4d38f308d0cccc5f28198d646ed136bc97
codeyzx <yahyatruth@gmail.com>
2024-06-04 20:33:04 +0700

    chore: Update page title and fix carousel styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
-        <img src="logo.png" alt="" width={25}/>
+        <img src="logo.png" alt="" width={25} />
         <span>B2-Proyek</span>
       </div>
       <div className="icons">
@@ -20,7 +20,7 @@ const Navbar = () => {
             src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
             alt=""
           />
-          <span>Yahya</span>
+          <span>User</span>
         </div>
         <img src="/settings.svg" alt="" className="icon" />
       </div>
