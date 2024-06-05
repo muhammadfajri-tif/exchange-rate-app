34f00c1d13aa86d0b0fb4ed08f8ab2a1dd348028
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-06-05 12:53:09 +0700

    reset

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,8 +8,6 @@ import { useContext, useEffect, useState } from "react";
 import { Context } from "../../context/Context";
 import { getResponse } from "../../config/gemini";
 import Table from "../../components/table/Table";
-import TopBox from "../../components/topBox/TopBox";
-import PieChartBox from "../../components/pieChartBox/PieChartBox";
 
 const Home = () => {
   const bankNotes: BarChartSeries = { title: "Bank Notes", banks: [] };
@@ -114,9 +112,6 @@ const Home = () => {
       <div className="box box8">
         <Carousel />
       </div>
-      <div className="box box1">
-        <TopBox />
-      </div>
       <div className="home_chart">
         <div className="box box7">
           <BarChart barChart={bankNotes} />
@@ -130,9 +125,6 @@ const Home = () => {
         <div className="box box7">
           <BarChart barChart={specialRates} />
         </div>
-      </div>
-      <div className="box box4">
-        <PieChartBox />
       </div>
       <div className="App">
         <Table />
